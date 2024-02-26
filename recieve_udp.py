c62184c604da92ac67f27298ec5d9d43eb9cc6a1
rnasyrov <rnasyrov@airtaero.com>
2024-02-26 13:12:39 +0300

    Изменил получение байтов

--- recieve_udp.py
+++ recieve_udp.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import time
 
 from logger import create_logger
 
@@ -31,7 +32,8 @@ def receive_udp_from_trainer(update_func):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
     print("Listening for UDP packets on {}:{}".format(UDP_IP, UDP_PORT))
-    str_format = '<BBBBBBBBiBBHfff'
+    str_format = "8s I B B B B B 3s f f f f"#'<BBBBBBBBiBBHfff'
+    
     while not stop_udp_flag:
         try:
             data, addr = sock.recvfrom(10240)  # Буфер 1024 байта, вы можете увеличить его при необходимости
@@ -39,8 +41,10 @@ def receive_udp_from_trainer(update_func):
             print ("Error")
 
         received_byte = struct.unpack(str_format, data)
-        if(len(received_byte) != 15):
+        if(len(received_byte) != 37):
             continue
+        
+        header, id, state, massHuman, ageHuman, lane, swimmingGroup, reserve, distance, time, speed, acceleration = unpacked_data
 
         header = ''
         for i in range(HEADER_START_BYTE, HEADER_END_BYTE):
@@ -49,22 +53,23 @@ def receive_udp_from_trainer(update_func):
         if header != client_header:
             continue
 
-        id = received_byte[ID_BYTES]
-        state = received_byte[STATE_BYTE]
-        lane = received_byte[LANE_BYTE]
+      #  id = received_byte[ID_BYTES]
+      #  state = received_byte[STATE_BYTE]
+      #  lane = received_byte[LANE_BYTE]
 
 
-        time = received_byte[TIME_BYTES]
-        distance = received_byte[DISTANCE_BYTES]
-        speed = received_byte[SPEED_BYTES]
-        update_func(lane, id, state, distance, time, speed)
+      #  time = received_byte[TIME_BYTES]
+      #  distance = received_byte[DISTANCE_BYTES]
+      #  speed = received_byte[SPEED_BYTES]
+        update_func(lane, id, state, distance, time, speed, acceleration)
 
 udp_logger = create_logger('udp.log', use_formatter=False)
 
-def update_func(lane, id, state, distance, time, speed):
+def update_func(lane, id, state, distance, time, speed, acceleration):
     global udp_logger
+    timestamp_ms = int(time.time() * 1000)  # Получаем текущий таймстемп в миллисекундах
     print(f"id:{id}, state: {state}, distance: {distance}, lane: {lane}, time: {time}, speed: {speed}")
-    udp_logger.info(f'{int(time*1000)} {int(speed*1000)} {int(distance*1000)}')
+    udp_logger.info(f'{timestamp_ms}  {int(time*1000)} {int(speed*1000)} {int(distance*1000)} {int(acceleration)}')
     pass
 
 if __name__ == '__main__':
