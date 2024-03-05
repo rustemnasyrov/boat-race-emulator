3965ca4539719c77f20b201f81211e384d846585
rust <rustemnasyrov@gmail.com>
2024-03-05 12:04:49 +0300

    Рефаторинг в части пакета  udp. Сделан класс для разборапакета

--- recieve_udp.py
+++ recieve_udp.py
@@ -7,55 +7,63 @@ from logger import create_logger
 
 stop_udp_flag = False
 
+
 def stop_udp_recieve():
     global stop_udp_flag
     stop_udp_flag = True
 
+
+class UdpPacket:
+    def __init__(self, data, addr):
+        self.data = data
+        self.addr = addr
+
+        str_format = "<8s I BBBB BB2s f f f f f f"
+        header, self.id, self.packetNumber, self.state, self.massHuman, self.ageHuman, self.swimmingGroup, self.lane, self.reserve, self.race_time, self.boatTime, self.distance, self.speed, self.acceleration, self.strokeRate = struct.unpack(str_format, self.data)
+
+        self.header = header.decode().replace('\0', '')
+
+
 udp_logger = create_logger('udp.log', use_formatter=False)
 start_time =  time.time()
 
-def receive_udp_from_trainer(update_func,  udp_address = ("192.168.137.1", 62222)):
+
+def receive_udp_from_trainer(update_func, udp_address=("192.168.137.1", 62222)):
     global stop_udp_flag
 
     UDP_IP = udp_address[0]
-    UDP_PORT =  udp_address[1]
+    UDP_PORT = udp_address[1]
 
     client_header = "BRTC102"
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
     print("Listening for UDP packets on {}:{}".format(UDP_IP, UDP_PORT))
-    str_format = "<8s I BBBB BB2s f f f f f f"
-    
+
     while not stop_udp_flag:
         try:
             data, addr = sock.recvfrom(10240)  # Буфер 1024 байта, вы можете увеличить его при необходимости
         except:
             print ("Error")
 
-        received_byte = struct.unpack(str_format, data)
-        if(len(received_byte) != 15):           # Количество элементов в пакете. Не байт
-            continue
-        
-        header, id, packetNumber, state, massHuman, ageHuman, swimmingGroup, lane , reserve, race_time, boatTime, distance, speed, acceleration, strokeRate = received_byte
+        udp_packet = UdpPacket(data, addr)
 
-        if header.decode().replace('\0', '') != client_header:
+        if udp_packet.header != client_header:
             continue
 
-        update_func(lane, id, packetNumber, state, distance, race_time, speed, acceleration, boatTime, addr)
+        update_func(udp_packet)
+
         time.sleep(0.001)
 
-       # timestamp_ms = int((time.time() - start_time) * 1000) 
-       # udp_logger.info(f'{timestamp_ms} {id} {packetNumber} {state} {int(race_time*1000)} {int(boatTime * 1000)} {int(distance*1000)} {int(speed*1000)} {int(acceleration * 1000)}')
 
-
-def update_func(lane, id, packetNumber, state, distance, race_time, speed, acceleration, boatTime, addr):
+def update_func(udp_packet):
     global udp_logger, start_time
 
-    timestamp_ms = int((time.time() - start_time) * 1000) # Получаем текущий таймстемп в миллисекундах
-    print(f"{timestamp_ms} {id} {packetNumber}, state: {state}, distance: {distance}, lane: {lane}, time: {race_time}, speed: {speed}, acceleration: {acceleration}")
-    udp_logger.info(f'{timestamp_ms} {id} {packetNumber} {state} {int(race_time*1000)} {int(boatTime * 1000)} {int(distance*1000)} {int(speed*1000)} {int(acceleration * 1000)}')
-    pass
+    timestamp_ms = int((time.time() - start_time) * 1000)  # Получаем текущий таймстемп в миллисекундах
+    print(f"{timestamp_ms} {udp_packet.id} {udp_packet.packetNumber}, state: {udp_packet.state}, distance: {udp_packet.distance}, lane: {udp_packet.lane}, time: {udp_packet.race_time}, speed: {udp_packet.speed}, acceleration: {udp_packet.acceleration}")
+    udp_logger.info(f'{timestamp_ms} {udp_packet.id} {udp_packet.packetNumber} {udp_packet.state} {int(udp_packet.race_time*1000)} {int(udp_packet.boatTime * 1000)} {int(udp_packet.distance*1000)} {int(udp_packet.speed*1000)} {int(udp_packet.acceleration * 1000)}')
+
 
 if __name__ == '__main__':
     receive_udp_from_trainer(update_func)
+
